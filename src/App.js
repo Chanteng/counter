dcdf3931ddd7af2b574a8c59dac88d512e13b21f
Chanteng <yrnchanteng@gmail.com>
2021-08-28 11:07:02 -0700

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import ClassCounter from './Components/ClassCounter';
+import React, {useState} from 'react';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { number: 0};
+
+function App() {
+  const [number, setNumber] = useState(0)
+  // const [num, setNum] = useState(0)
+
+  function AddOne(){
+   setNumber (number + 1)
   }
 
-  AddOne() {
-    this.setState({
-      number: this.state.number + 1 
-    })
+  function SubOne() {
+    setNumber ( number - 1)
   }
 
-  SubtractOne() {
-    this.setState({
-      number: this.state.number - 1 
-    })
-  }
+ return (
+   <div>
+     <h1>{number}</h1>
+     <button onClick={() => AddOne()}>Add 1</button>
+     <button onClick={() => SubOne()}>Sub 1</button>
+   </div>
+ )
+}
+
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = { number: 0};
+//   }
+
+//   AddOne() {
+//     this.setState({
+//       number: this.state.number + 1 
+//     })
+//   }
+
+//   SubtractOne() {
+//     this.setState({
+//       number: this.state.number - 1 
+//     })
+//   }
 
  
-  render() {
-    return (
-      <div>
-        <h1>{this.state.number}</h1>
-        <button onClick={() => this.AddOne()}>Add 1</button>
-        <button onClick={() => this.SubtractOne()}>Sub 1</button>
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.number}</h1>
+//         <button onClick={() => this.AddOne()}>Add 1</button>
+//         <button onClick={() => this.SubtractOne()}>Sub 1</button>
        
-      </div>
-    );
-  }
-}
+//       </div>
+//     );
+//   }
+// }
 
 
 
